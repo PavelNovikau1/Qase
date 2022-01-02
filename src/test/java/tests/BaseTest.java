d967b1e993b55e0ce813e5ccf72a8d5bf1bbdbac
Pavel <razor8ne8@icloud.com>
2022-01-02 16:46:18 +0200

    Added Property reader to the project.

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,17 +3,16 @@ package tests;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.testng.annotations.BeforeMethod;
+import utils.PropertyReader;
 import static io.restassured.RestAssured.given;
 
 public abstract class BaseTest {
-
-    private static final String TOKEN = "1ab078fbcf8806977c0764557e279901a47d3969";
 
     @BeforeMethod
     public void configureRestAssured() {
         RestAssured.baseURI = "https://api.qase.io/v1";
         RestAssured.requestSpecification = given()
-                .header("Token", TOKEN)
+                .header("token", System.getProperty("token", PropertyReader.getProperty("token")))
                 .contentType(ContentType.JSON).accept(ContentType.JSON);
     }
 }
